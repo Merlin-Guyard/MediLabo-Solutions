170833a322502d240cc9567f6dba0f8b43283c4a
Merlin Guyard <merlin.guyard@gmail.com>
2023-10-12 22:28:43 +0200

    Added gateway project

---
+++ medilabosolution-gateway/src/main/java/com/oc/medilabosolutiongateway/MedilabosolutionGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.oc.medilabosolutiongateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilabosolutionGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilabosolutionGatewayApplication.class, args);
+	}
+
+}
---
+++ medilabosolution-gateway/src/test/java/com/oc/medilabosolutiongateway/MedilabosolutionGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oc.medilabosolutiongateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedilabosolutionGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

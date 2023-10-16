ff0a58060eb1d3715de0f2bdb81e5aadc110436c
Merlin Guyard <merlin.guyard@gmail.com>
2023-10-16 22:29:18 +0200

    Added 4 patient for testing purposes

--- medilabo-backend/src/main/java/com/medilabosolutions/backend/controller/PatientController.java
+++ medilabo-backend/src/main/java/com/medilabosolutions/backend/controller/PatientController.java
@@ -24,17 +24,48 @@ public class PatientController {
         return "Application online !";
     }
 
-    @RequestMapping("/add")
-    public void addPatient() {
-        Patient patient = new Patient(
-                "John",
-                "Doe",
-                "1990-05-15",
-                "Male",
-                "75000",
-                "555-123-4567"
+    @RequestMapping("/test")
+    public void addPatients() {
+        Patient patient1 = new Patient(
+                "TestNone",
+                "Test",
+                "1966-12-31",
+                "F",
+                "1 Brookside St",
+                "100-222-3333"
         );
-        patientService.addPatient(patient);
+
+        Patient patient2 = new Patient(
+                "TestBorderline",
+                "Test",
+                "1945-06-24",
+                "M",
+                "2 High St",
+                "200-333-4444"
+        );
+
+        Patient patient3 = new Patient(
+                "TestInDanger",
+                "Test",
+                "2004-06-18",
+                "M",
+                "3 Club Road",
+                "300-444-5555"
+        );
+
+        Patient patient4 = new Patient(
+                "TestEarlyOnset",
+                "Test",
+                "2002-06-28",
+                "F",
+                "4 Valley Dr",
+                "400-555-6666"
+        );
+
+        patientService.addPatient(patient1);
+        patientService.addPatient(patient2);
+        patientService.addPatient(patient3);
+        patientService.addPatient(patient4);
     }
 
     @RequestMapping("/get")

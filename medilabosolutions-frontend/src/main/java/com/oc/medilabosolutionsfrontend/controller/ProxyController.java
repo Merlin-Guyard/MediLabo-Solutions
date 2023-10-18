aa684e6b7b3cf5c5a7b3065022b66e1e5b95df4c
Merlin Guyard <merlin.guyard@gmail.com>
2023-10-18 23:18:39 +0200

    separated frontend controller in 2
    one for backend communication, another for pages.
    commented custom user

--- medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/controller/ProxyController.java
+++ medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/controller/ProxyController.java
@@ -2,17 +2,29 @@ package com.oc.medilabosolutionsfrontend.controller;
 
 import com.oc.medilabosolutionsfrontend.service.ProxyService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequestMapping("/frontend")
-public class PageController {
+public class ProxyController {
+
+    @Autowired
+    private final ProxyService proxyService;
+
+    public ProxyController(ProxyService proxyService) {
+        this.proxyService = proxyService;
+    }
 
     @GetMapping("/login")
     public String loginPage() {
         return "login";
     }
+
+    @RequestMapping("/getUser")
+    public void getUser() {
+        proxyService.getUsers("doctor");
+    }
+
 }
--- medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/service/CustomUserDetailsService.java
+++ medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/service/CustomUserDetailsService.java
@@ -1,26 +1,26 @@
-package com.oc.medilabosolutionsfrontend.service;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.authority.SimpleGrantedAuthority;
-import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.stereotype.Service;
-
-import java.util.Collections;
-import java.util.Optional;
-
-@Service
-public class CustomUserDetailsService implements UserDetailsService {
-
-    @Autowired
-    private ProxyService backendUserService;
-
-    public UserDetails loadUserByUsername(String username) {
-        Optional<User> oUser = userRepository.findByUsername(username);
-        if(oUser.isEmpty()) {
-            throw new UsernameNotFoundException("User " +username+ " not found");
-        }
-        return new org.springframework.security.core.userdetails.User(oUser.get().getUsername(), oUser.get().getPassword(), Collections.singletonList(new SimpleGrantedAuthority(oUser.get().getRole())));
-    }
-}
+//package com.oc.medilabosolutionsfrontend.service;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.security.core.authority.SimpleGrantedAuthority;
+//import org.springframework.security.core.userdetails.UserDetails;
+//import org.springframework.security.core.userdetails.UserDetailsService;
+//import org.springframework.security.core.userdetails.UsernameNotFoundException;
+//import org.springframework.stereotype.Service;
+//
+//import java.util.Collections;
+//import java.util.Optional;
+//
+//@Service
+//public class CustomUserDetailsService implements UserDetailsService {
+//
+//    @Autowired
+//    private ProxyService backendUserService;
+//
+//    public UserDetails loadUserByUsername(String username) {
+//        Optional<User> oUser = userRepository.findByUsername(username);
+//        if(oUser.isEmpty()) {
+//            throw new UsernameNotFoundException("User " +username+ " not found");
+//        }
+//        return new org.springframework.security.core.userdetails.User(oUser.get().getUsername(), oUser.get().getPassword(), Collections.singletonList(new SimpleGrantedAuthority(oUser.get().getRole())));
+//    }
+//}

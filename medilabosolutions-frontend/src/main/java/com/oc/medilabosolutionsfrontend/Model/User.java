2617c718cf42636890cfd6066fbb3a075727be6e
Merlin Guyard <merlin.guyard@gmail.com>
2023-10-24 15:16:53 +0200

    Removed spring security from front (need to install it in gateway)
    Changed gateway routes as well

--- medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/Model/User.java
+++ medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/Model/User.java
@@ -2,24 +2,15 @@ package com.oc.medilabosolutionsfrontend.Model;
 
 public class User {
 
-    private String id;
     private String username;
     private String password;
-    private String role;
 
-    public User(String id, String username, String password, String role) {
-        this.id = id;
+    public User(String username, String password) {
         this.username = username;
         this.password = password;
-        this.role = role;
     }
 
-    public String getId() {
-        return id;
-    }
-
-    public void setId(String id) {
-        this.id = id;
+    public User() {
     }
 
     public String getUsername() {
@@ -38,11 +29,5 @@ public class User {
         this.password = password;
     }
 
-    public String getRole() {
-        return role;
-    }
 
-    public void setRole(String role) {
-        this.role = role;
-    }
 }
--- medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/configuration/SpringSecurityConfig.java
+++ medilabosolutions-frontend/src/main/java/com/oc/medilabosolutionsfrontend/configuration/SpringSecurityConfig.java
@@ -1,51 +1,51 @@
-package com.oc.medilabosolutionsfrontend.configuration;
-
-import com.oc.medilabosolutionsfrontend.service.CustomUserDetailsService;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.SecurityFilterChain;
-
-@Configuration
-@EnableWebSecurity
-public class SpringSecurityConfig {
-
-
-    @Autowired
-    private CustomUserDetailsService customUserDetailsService;
-
-    //Password Encoder
-    @Bean
-    public PasswordEncoder passwordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    //Login Security configuration
-    @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-
-        return http
-                .authorizeHttpRequests(auth -> auth
-                        .requestMatchers("/frontend/login","/frontend/home").permitAll()
-                        .requestMatchers("/frontend/home").hasAnyRole("USER")
-                        .anyRequest().authenticated()
-                )
-
-                .formLogin((form) -> form
-                        .loginPage("/frontend/login")
-                        .permitAll()
-                        .defaultSuccessUrl("/frontend/home"))
-
-                .userDetailsService(customUserDetailsService)
-
-                .logout((logout) -> logout
-                        .logoutUrl("/login"))
-
-                .build();
-    }
-
-}
+//package com.oc.medilabosolutionsfrontend.configuration;
+//
+//import com.oc.medilabosolutionsfrontend.service.CustomUserDetailsService;
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+//import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+//import org.springframework.security.crypto.password.PasswordEncoder;
+//import org.springframework.security.web.SecurityFilterChain;
+//
+//@Configuration
+//@EnableWebSecurity
+//public class SpringSecurityConfig {
+//
+//
+//    @Autowired
+//    private CustomUserDetailsService customUserDetailsService;
+//
+//    //Password Encoder
+//    @Bean
+//    public PasswordEncoder passwordEncoder() {
+//        return new BCryptPasswordEncoder();
+//    }
+//
+//    //Login Security configuration
+//    @Bean
+//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+//
+//        return http
+//                .authorizeHttpRequests(auth -> auth
+//                        .requestMatchers("/frontend/login","/frontend/home").permitAll()
+//                        .requestMatchers("/frontend/home").hasAnyRole("USER")
+//                        .anyRequest().authenticated()
+//                )
+//
+//                .formLogin((form) -> form
+//                        .loginPage("/frontend/login")
+//                        .permitAll()
+//                        .defaultSuccessUrl("/frontend/home"))
+//
+//                .userDetailsService(customUserDetailsService)
+//
+//                .logout((logout) -> logout
+//                        .logoutUrl("/login"))
+//
+//                .build();
+//    }
+//
+//}

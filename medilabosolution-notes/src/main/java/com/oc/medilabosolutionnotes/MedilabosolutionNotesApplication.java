06e865100e084649202d99a129c5b182d83e5792
Merlin GUYARD <merlin.guyard@gmail.com>
2023-12-15 02:03:52 +0100

    Added project

---
+++ medilabosolution-notes/src/main/java/com/oc/medilabosolutionnotes/MedilabosolutionNotesApplication.java
@@ -0,0 +1,13 @@
+package com.oc.medilabosolutionnotes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedilabosolutionNotesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedilabosolutionNotesApplication.class, args);
+	}
+
+}
